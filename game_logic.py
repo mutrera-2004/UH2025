ddc4ab8b5ea23e53a8c45c0858cb7b1a8a43653c
Mutrera <mutrera@uchicago.edu>
2025-03-29 14:19:01 -0500

    Game logic file

---
+++ game_logic.py
@@ -0,0 +1,13 @@
+# create the classes for zombies/player/bullet
+import pygame
+
+class Player:
+    '''
+    Class representing a Player
+
+    Attributes:
+        - life [int]: percentage of health left
+        - bulelts [int]: number of bullets left on the gun
+        - position [pygame.Rect]: position of the object
+    '''
+    
