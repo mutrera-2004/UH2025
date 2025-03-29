4dde33744caceb62e2b54edd95160d211f94a26c
Brian Barragan-Cid <bbarragancid23@DESKTOP-JKM1DL4>
2025-03-29 15:44:45 -0500

    MOVEMENTgit statusgit status!

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pygame
 
 import map
+import game_logic
 
 # Tile map with walls represented by 'W' and empty spaces by '.'
 test_map = [
@@ -18,15 +19,18 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 running = True
 black = (0, 0, 0)
 
+
+test_player = game_logic.Player(100, pygame.rect.Rect(WIDTH // 2, HEIGHT // 2, TILE_SIZE, TILE_SIZE))
 test = map.Map(test_map, TILE_SIZE)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    test.update(event)
     screen.fill(black)
-    for tile in test.tiles:
-        tile.draw(screen, TILE_SIZE)
+    test.draw(screen, WIDTH, HEIGHT, TILE_SIZE)
+    test_player.draw(screen)
     pygame.display.flip()
 
 
