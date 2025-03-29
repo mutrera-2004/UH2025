b3be47b1058b614074835c287e0c1964a72f6ea1
Brian Barragan-Cid <bbarragancid23@DESKTOP-JKM1DL4>
2025-03-29 14:53:53 -0500

    Base Map sort of

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 import pygame
 
-import pygame
-screen = pygame.display.set_mode((950, 950))
+import map
+
+# Tile map with walls represented by 'W' and empty spaces by '.'
+test_map = [
+    "..W..W",
+    ".W...W",
+    ".W...W"
+]
+
+WIDTH = 960
+HEIGHT = 640
+
+TILE_SIZE = 64
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 running = True
 black = (0, 0, 0)
+
+test = map.Map(test_map, TILE_SIZE)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill(black)
+    for tile in test.tiles:
+        tile.draw(screen, TILE_SIZE)
     pygame.display.flip()
+
 
 pygame.quit()
