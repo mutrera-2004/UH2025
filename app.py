0f93958e30d56c722144bc1add133cb8c46964a1
Brian Barragan-Cid <bbarragancid23@DESKTOP-JKM1DL4>
2025-03-29 14:15:49 -0500

    Basic Pygame

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import pygame
 
+import pygame
+screen = pygame.display.set_mode((950, 950))
+
+running = True
+black = (0, 0, 0)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(black)
+    pygame.display.flip()
+
+pygame.quit()
