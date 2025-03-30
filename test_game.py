462432ddb84c72b150aa337b48c28ab184b83955
Mutrera <mutrera@uchicago.edu>
2025-03-30 04:24:25 -0500

    please

--- test_game.py
+++ test_game.py
@@ -2,8 +2,7 @@ import random
 import pygame
 from pygame import mixer
 import map
-import os
-from player import Player 
+from player import Player
 from bullet import Bullet
 from zombies import Zombie
 import config
@@ -11,26 +10,49 @@ from game import Game
 from pytmx.util_pygame import load_pygame
 
 
+
+
+# Tile map with walls represented by 'W' and empty spaces by '.'
+pygame.init()
+test_map = [
+    "..W..W",
+    ".W...W",
+    ".W...W"
+]
 screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
+
+
 
 
 mapp = load_pygame(r'data/UH Map.tmx')
 
+
 running = True
 black = (0, 0, 0)
 
+
+curr_wave = 1
+new_wave = True
+zombies_per_wave = [0, 10, 20, 30] # number of zombies that are spawned per wave and 3 waves only NOT 0 index
 player = Player(100, config.PLAYER_RECT)
 player._position.center = (config.WIDTH // 2, config.HEIGHT // 2)
 test = map.Map(mapp)
 bullets = pygame.sprite.Group()
 zombies = pygame.sprite.Group()
-test_game = Game(test, player)
+# test_game = Game(test, player)
+# test_game.spawn_zombie()
 previous_time = pygame.time.get_ticks()
+
+
+test_game = Game(test, player)
+
 
 while pygame.time.get_ticks() - previous_time <= 300:
     continue
 
+
 previous_time = pygame.time.get_ticks()
+
 
 def generate_fog():
     dark_surface = pygame.Surface((config.WIDTH, config.HEIGHT))
@@ -43,37 +65,23 @@ def generate_fog():
     dark_surface.blit(config.glow, glow_rect)
     screen.blit(dark_surface, (0, 0), special_flags=pygame.BLEND_RGB_MULT)
 
+
 def zombie_healthbars(zombies):
     for zombie in zombies:
         # Draw health bars for zombies
         health_bar_width = 40
         health_bar_height = 6
         health_bar_pos = (zombie._rect.centerx - health_bar_width//2, zombie._rect.bottom + 5)
-        
+       
         # Background/empty bar (red)
         pygame.draw.rect(screen, (255, 0, 0),
                         (health_bar_pos[0], health_bar_pos[1], health_bar_width, health_bar_height))
-        
-        # Filled portion of health bar (green) 
+       
+        # Filled portion of health bar (green)
         health_percentage = zombie._health / 100
         current_health_width = health_bar_width * health_percentage
         pygame.draw.rect(screen, (0, 255, 0),
                         (health_bar_pos[0], health_bar_pos[1], current_health_width, health_bar_height))
-        
-
-def draw_final_message():
-    """Draws the final message sprite at the bottom of the screen."""
-    # Load the sprite from the "sprites" folder
-    sprite_path = os.path.join("sprites", "HELP.png")
-    final_message = pygame.image.load(sprite_path).convert_alpha()
-    
-    # Get the screen dimensions
-    screen_width, screen_height = screen.get_size()
-    final_message = pygame.transform.scale(final_message, (screen_width, 150))
-    sprite_rect = final_message.get_rect(midbottom=(screen_width // 2, screen_height))
-    
-    # Draw the sprite on the screen
-    screen.blit(final_message, sprite_rect)
 
 
 def player_healthbar():
@@ -81,21 +89,23 @@ def player_healthbar():
     health_bar_width = 300
     health_bar_height = 8
     health_bar_pos = (10, 10)  # Fixed position in top left
-    
+   
     # Background/empty bar (red)
-    pygame.draw.rect(screen, (255, 0, 0), 
+    pygame.draw.rect(screen, (255, 0, 0),
                     (health_bar_pos[0], health_bar_pos[1], health_bar_width, health_bar_height))
-    
+   
     # Filled portion of health bar (green)
     health_percentage = player.health / 100
     current_health_width = health_bar_width * health_percentage
     pygame.draw.rect(screen, (0, 255, 0),
                     (health_bar_pos[0], health_bar_pos[1], current_health_width, health_bar_height))
-    
+   
     # Draw percentage text
     font = pygame.font.Font(None, 24)
     percentage_text = font.render(f"{round(health_percentage * 100)}%", True, (255, 255, 255))
     screen.blit(percentage_text, (health_bar_pos[0] + health_bar_width + 5, health_bar_pos[1]))
+
+
 
 
 def spawn_zombie(num_zombies: int, groups: pygame.sprite.Group):
@@ -119,17 +129,8 @@ def spawn_zombie(num_zombies: int, groups: pygame.sprite.Group):
         zombie = Zombie(30, zombie_rect.center, groups)
         zombie_counter -= 1
 
-mixer.init()
-sound1 = mixer.Sound("./audio/background.mp3")
-sound2 = mixer.Sound("./audio/zombie.mp3")
-sound3 = mixer.Sound("./audio/shotgun.mp3")
 
-channel1 = mixer.Channel(0)
-channel2 = mixer.Channel(1)
-channel3 = mixer.Channel(2)
 
-channel1.play(sound1, -1)
-channel2.play(sound2, -1)
 
 while running:
     for event in pygame.event.get():
@@ -142,13 +143,17 @@ while running:
             mixer.init()
             mixer.music.load("./audio/shotgun.mp3")
             mixer.music.play()
+   
+    if new_wave:
+        spawn_zombie(zombies_per_wave[curr_wave], zombies)
+        new_wave = False
+
 
     if not zombies:
         curr_wave += 1
         new_wave = True
         if curr_wave > 3:
             test.good_ending = True
-        sound2.stop()
 
 
     test.update()
@@ -159,15 +164,14 @@ while running:
         bullet.update()
         bullet.draw(screen)
 
+
     for zombie in zombies:
         zombie.draw(screen)
         zombie.update(test.offset_x, test.offset_y, player)
         zombie.move(test)
-    
+   
     zombie_healthbars(zombies)
     generate_fog()
     player_healthbar()
-    if test_game.good_ending:
-        draw_final_message()
-        
+       
     pygame.display.flip()
