0a0571e3e049ae9725f42f092d42dfb37c8e63ab
Brian Barragan-Cid <bbarragancid23@DESKTOP-JKM1DL4>
2025-03-29 19:20:43 -0500

    small fix

--- game_logic.py
+++ game_logic.py
@@ -47,7 +47,7 @@ class Player:
         self._num_bullets = bullets
         self._curr_bullets = []
         self._position = position
-        self.direction = "right"
+        self._direction = "right"
 
     @property
     def life(self):
